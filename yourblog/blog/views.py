86dc70a1a04049f9ada4814a09023bcc64e22ddf
= <=>
2023-05-17 21:04:26 +0300

    created post_share view

--- yourblog/blog/views.py
+++ yourblog/blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -23,3 +24,18 @@ def post_detail(request, year, month, day, post):
     )
     context = {'post': post}
     return render(request, 'blog/post/detail.html', context)
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid:
+            form.cleaned_data
+    else:
+        form = EmailPostForm()
+    context = {
+        'post': post,
+        'form': form
+    }
+    return render(request, 'blog/post/share.html', context)
