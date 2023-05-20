62117f433936e2584c5c5de1a2337fda615b0c6a
= <=>
2023-05-20 13:49:20 +0300

    created similar_posts filter and added it to post_detail and added render of it to detail.html

--- yourblog/blog/views.py
+++ yourblog/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
@@ -38,11 +39,20 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    # список активных комментариев к посту.
     comments = post.comments.filter(active=True)
+    # форма для комментария пользователя.
     form = CommentForm()
+    # список схожих постов.
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
     context = {'post': post,
                'form': form,
-               'comments': comments}
+               'comments': comments,
+               'similar_posts': similar_posts}
     return render(request, 'blog/post/detail.html', context)
 
 
